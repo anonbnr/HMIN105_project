6e05ece1c03d37377a044650cdd261a871747299
AnonBNR <bashar.nabil.rima@gmail.com>
2018-11-29 17:58:21 +0100

    (server) modularized functions; removed IPC_init and placed initialization tasks in server

--- server.h
+++ server.h
@@ -1,15 +1,38 @@
 #ifndef _SERVER_H
 #define _SERVER_H
 
-#define MSG_SIZE 1024
-#define PSEUDO_SIZE 100
+#include <sys/types.h>
+#include "init_IPC.h"
+
+#define SHM_WB_CODE 66
+#define SHM_CLIENTS_CODE 99
+#define SEM_CODE 33
 #define PORT_NUMBER 31000
 #define BACKLOG 10
 
-/*structure du message reçu par le client*/
-typedef struct message {
-  char text[MSG_SIZE];
-  char pseudo[PSEUDO_SIZE];
-} message;
+/*STRUCTURE*/
+//whiteboard structure
+typedef struct whiteboard {
+  char content[WHITEBOARD_SIZE];
+} whiteboard;
+
+//union structure for semaphores
+union semun {
+  int value; /*valeur quand cmd=SETVAL*/
+  struct semid_ds *buf; /*tampon quand cmd=IPC_STAT ou cmd=IPC_SET*/
+  unsigned short *array; /*tableau quand cmd=GETALL ou cmd=SETALL*/
+  struct seminfo *__buf; /*tampon quand cmd=IPC_INFO (sous Linux)*/
+};
+
+/*FUNCTIONS*/
+key_t ipc_key_generation(const char* file, int code, const char* error_msg);
+int shm_id_reception(key_t shm_key, size_t size, int permissions, const char* error_msg);
+void *shm_attachment(int shm_id, const void *shmaddr, int permissions, const char* error_msg);
+int sem_id_reception(key_t sem_key, size_t sems, int permissions, const char* error_msg);
+whiteboard* init_wb();
+int* init_shm_clients();
+int init_sem();
+union semun init_sem_union(int sem_id);
+void init_IPC(whiteboard *wb, int *shm_clients, int *sem_id, union semun *unisem);
 
 #endif //_SERVER_H
